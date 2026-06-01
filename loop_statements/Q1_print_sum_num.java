6574bbacc8950dd4ea3a1771badb3a07c39c70d6
kusum1905 <kusumrathore4141.in>
2026-06-01 18:52:27 +0530

    loop practice : sum of N numbers & print square pattern

---
+++ loop_statements/Q1_print_sum_num.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Q1_print_sum_num {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        int i = 1;
+        while(i <= n) {
+            sum = sum + i;
+            i++;
+        }
+        System.out.println("sum is :"+ sum);
+    }
+}
