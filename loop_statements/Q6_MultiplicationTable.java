ded3276c709cf716318780ddbd5f6b96182c6bf7
kusum1905 <kusumrathore4141.in>
2026-06-04 20:53:31 +0530

    print multiplication Table of number N

---
+++ loop_statements/Q6_MultiplicationTable.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Q6_MultiplicationTable {
+    public static void printMultiplicationTable(int number) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number:");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=10; i++) {
+            System.out.println(n+ "*"+i+"="+ n*i);
+        }
+    }
+    public static void main(String[] args) {
+        printMultiplicationTable(n);
+    }
+}
