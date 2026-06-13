7a51b9d26d6aa48651f81791d97cf9b0df983f7b
kusum1905 <kusumrathore4141.in>
2026-06-13 19:11:43 +0530

    sum of the digits in an integer

--- functions_methods/PQ.java
+++ functions_methods/PQ.java
@@ -1,19 +1,21 @@
-// Q1/PQ
+import java.util.Scanner;                      //import scanner class to take input from user
+public class PQ {
+    public static void main(String[]args) {    //execution starts from the main method
+    Scanner sc = new Scanner(System.in);
+    System.out.print("input first integer :");
+    int digits = sc.nextInt();
+    System.out.println("the sum of integer"+sumDigits(digits));
 
-import java.util.*;
-public class PQ{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter first input:");
-        double x = sc.nextDouble();
-        System.out.print("enter second input:");
-        double y = sc.nextDouble();
-        System.out.print("enter third input:");
-        double z = sc.nextDouble();
-
-        System.out.println("Average is"+average(x,y,z));
     }
-    public static double average(double x,double y,double z) {
-        return (x+y+z)/3;
+
+    public static int sumDigits(int n) {
+    int sumDigits = 0;
+    while(n>0) {
+        int lastDigit = n%10;
+        sumDigits += lastDigit;
+        n = n/10;
+    }
+    return sumDigits;
+
     }
 }
